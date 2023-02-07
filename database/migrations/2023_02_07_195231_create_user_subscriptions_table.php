7220722ae63c6c5880a174cd90b105d112c3b488
Hasen Suadik <hasensuadik39@gmail.com>
2023-02-07 22:58:28 +0300

    model and migration created successfully

---
+++ database/migrations/2023_02_07_195231_create_user_subscriptions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('start_date');
+            $table->date('expired_date');
+            $table->boolean('status');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_subscriptions');
+    }
+};
